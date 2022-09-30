b210c2f4678a199bb9c58da3aca732af03e017a8
Karniela <095282kitty@gmail.com>
2022-09-30 16:33:15 +0800

    unpin host

--- hw2/main.js
+++ hw2/main.js
@@ -1,13 +1,17 @@
 const host_pic =  document.getElementById("host_pic").src;
-
-
+const collaborators = document.getElementById("Collaborators");
+const Spotlight =document.getElementById("Spotlight");
 
 // function to remove guest 
 function RemoveGuest(e){
     e.parentNode.parentNode.remove();
 }
 
+function RemoveSpotlight(e){
+    collaborators.style.width = ('100%');  
+    Spotlight.style.display = "none";
 
+}
 // if the pin button is toggled, exchange the name and picture, and disable the X button
 function ChangeSpotlight(e){
     let Spotlight_name = document.querySelector(".Spotlight_id").innerHTML;
@@ -15,10 +19,9 @@ function ChangeSpotlight(e){
         console.log(e.parentNode.previousElementSibling.firstChild);
         e.parentNode.previousElementSibling.firstChild.nextElementSibling.visibility = 'hidden';
         e.parentNode.previousElementSibling.firstChild.nextElementSibling.disabled = true;
-        console.log('This is host');
         }
     else{
-        console.log(e.parentNode.previousElementSibling.firstChild);
+       
         e.parentNode.previousElementSibling.firstChild.nextElementSibling.visibility = 'visible';
         e.parentNode.previousElementSibling.firstChild.nextElementSibling.disabled = false;
     }
