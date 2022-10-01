2510b321cd32659a621604d8c829289bf95e005b
Karniela <095282kitty@gmail.com>
2022-10-01 20:11:07 +0800

    Extra requirements done

--- hw2/README.md
+++ hw2/README.md
@@ -2,9 +2,17 @@
 
 Basic Requirements:
 1. Exit sign for each participant
-2. Extra three buttons to pin the participant
-3. Unpin the participant and rearrange the window
+    - please notice that the exit sign is not enbled when guest is in spotlight(This can be self-defined, according to Prof.Huang.)
+2. Extra three buttons to pin the participant to spotlight
+3. Extra three buttons to pin the participant.
+4. When the participant is unpinned, window would be rearranged.
+5. If all participants are exited, the host would be back to spotlight.
 
 Advanced Requirements:
 1. Show current time
-2. 
+2. Show random number of guests in the beginning(number ranges from 1 to 15)
+3. Add new participants with the icon down below(next to the information icon)
+    - please DO NOT enter same name for each user
+    - please enter photo url for profile image
+    - if your are too lazy to look for photos, here's the link to get them: https://spy-family.net/special/special13.php
+4. Dynamically change the width of the guests' divs to make it looks better
--- hw2/main.js
+++ hw2/main.js
@@ -3,6 +3,7 @@ const collaborators = document.getElementById("Collaborators");
 const Spotlight =document.getElementById("Spotlight");
 const Guests = document.getElementsByClassName("Guest");
 const Host = document.getElementById("host_section");
+const number_of_guests = getRandomInt(16);
 var Spotlight_Exist = true;
 var Today = new Date;
 
@@ -28,10 +29,42 @@ function RemoveGuest(e){
         collaborators.display = "none";
         collaborators.querySelector(".Guest").remove(); 
         document.querySelector(".Spotlight_Pic").src =  "https://spy-family.net/assets/img/special/anya/01.png";
-        document.querySelector(".Spotlight_id").innerHTML = "你";
+        document.querySelector(".Spotlight_id").innerHTML = "你"; 
     }
+    var all_participants = collaborators.querySelectorAll(".Guest");
+    var participants_num = all_participants.length;
+    var last_index = participants_num-1;
+    var last_two_index = participants_num-2;
+    if (participants_num%2 !== 0 && Spotlight_Exist === true){
+            for (var i = 0; i < participants_num; i++) {
+            console.log("change width");
+            all_participants[i].style.width = "47%";
+            }
+            all_participants[last_index].style.width = "75%";
+        }
+    if (participants_num%3 === 1 && Spotlight_Exist === false){
+            for (var i = 0; i < participants_num; i++) {
+                console.log("change width");
+                all_participants[i].style.width = "30%";
+            }
+            all_participants[last_index].style.width = "50%";
+        }else if(participants_num%3 === 2 && Spotlight_Exist === false){
+            for (var i = 0; i < participants_num; i++) {
+                console.log("change width");
+                all_participants[i].style.width = "30%";
+            }
+            all_participants[last_index].style.width = "40%";
+            all_participants[last_two_index].style.width = "40%"
+        }else if(participants_num%3 === 0 && Spotlight_Exist === false){
+            for (var i = 0; i < participants_num; i++) {
+                console.log("change width");
+                all_participants[i].style.width = "30%";
+            }
+            
+        }
 }
 
+// funtion to remove current spotlight and change into grid view
 function RemoveSpotlight(e){
     var colab1 = document.querySelector('.Guest');
     var colab_clone = colab1.cloneNode(true);
@@ -52,14 +85,25 @@ function RemoveSpotlight(e){
         Guests[i].style.width=("30%");    
         Guests[i].style.height=("40%");
         }
-   
     collaborators.style.width = ('100%');  
     collaborators.style.height = ('90%');
     Spotlight.style.display = "none";
     Spotlight_Exist = false;
+
+    const all_participants = collaborators.querySelectorAll(".Guest");
+    const participants_num = all_participants.length;
+    var last_index = participants_num-1;
+    var last_two_index = participants_num-2;
+    
+    if (participants_num%3 === 1 && Spotlight_Exist === false){
+            all_participants[last_index].style.width = "50%";
+        }else if(participants_num%3 === 2 && Spotlight_Exist === false){
+            all_participants[last_index].style.width = "40%";
+            all_participants[last_two_index].style.width = "40%"
+        }
 }
 
-// if the pin button is toggled, exchange the name and picture, and disable the X button
+// If the pin button is toggled, exchange the name and picture, and disable the X button
 function ChangeSpotlight(e){
     let Spotlight_name = document.querySelector(".Spotlight_id").innerHTML;
     if (Spotlight_Exist === true){
@@ -73,6 +117,9 @@ function ChangeSpotlight(e){
     e.previousElementSibling.src = Spotlight_pic;
     document.querySelector(".Spotlight_id").innerHTML = e.parentNode.nextElementSibling.innerHTML;
     e.parentNode.nextElementSibling.innerHTML = Spotlight_name;
+
+
+
     }else{
         Spotlight.style.display = ("initial");
         collaborators.style.width= ("27%");
@@ -81,7 +128,77 @@ function ChangeSpotlight(e){
         console.log(e.parentNode.parentNode);
         e.parentNode.parentNode.remove();
         Spotlight_Exist = true;
-    }
-    
-     
+    }   
 }
+
+// function to add new participants
+function AddPeople(e){
+    var colab1 = document.querySelector('.Guest');
+    if (collaborators.querySelectorAll(".Guest").length===0){
+        alert('You just kicked everyone out. You deserve to be alone:)');
+    }else{
+        var nickname = prompt("Please enter your nickname");
+        var profile_image = prompt("Please choose your profile image(enter link)", "https://spy-family.net/assets/img/special/summermission04/05.png");
+        var colab_clone = colab1.cloneNode(true);
+        colab_clone.id = nickname;
+        colab_clone.firstChild.nextElementSibling.nextElementSibling.firstChild.nextElementSibling.src = profile_image;
+        colab_clone.firstChild.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML = nickname;
+        colab1.after(colab_clone);
+        
+        const all_participants = collaborators.querySelectorAll(".Guest");
+        const participants_num = all_participants.length;
+        var last_index = participants_num-1;
+        var last_two_index = participants_num-2;
+
+        if (participants_num%2 !== 0 && Spotlight_Exist === true){
+            for (var i = 0; i < participants_num; i++) {
+                console.log("change width");
+                all_participants[i].style.width = "47%";
+                }
+                
+                all_participants[last_index].style.width = "75%";
+            }
+        if (participants_num%3 === 1 && Spotlight_Exist === false){
+            for (var i = 0; i < participants_num; i++) {
+                console.log("change width");
+                all_participants[i].style.width = "30%";
+                }
+            all_participants[last_index].style.width = "50%";
+        }else if(participants_num%3 === 2 && Spotlight_Exist === false){
+            for (var i = 0; i < participants_num; i++) {
+                console.log("change width");
+                all_participants[i].style.width = "30%";
+            }
+            all_participants[last_index].style.width = "40%";
+            all_participants[last_two_index].style.width = "40%"
+        }else if(participants_num%3 === 0 && Spotlight_Exist === false){
+            for (var i = 0; i < participants_num; i++) {
+                console.log("change width");
+                all_participants[i].style.width = "30%";
+            }
+                
+            }
+    }
+}
+
+// show random number of participant
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+function show_random_number(){
+    
+    const all_participants = collaborators.querySelectorAll(".Guest");
+    const participants_num = all_participants.length;
+    console.log(number_of_guests);
+    console.log(all_participants.length);   
+    for (var i = number_of_guests; i < participants_num; i++) {
+        all_participants[i].remove();
+        }
+    if (number_of_guests%2 !== 0){
+        var last_index = number_of_guests-1;
+        all_participants[last_index].style.width = "75%";
+    }
+
+}
+show_random_number();
