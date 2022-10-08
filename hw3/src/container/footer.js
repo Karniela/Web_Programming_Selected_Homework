b05dc4415806eab4adad6f3553068dc5fcf2abf5
Karniela <095282kitty@gmail.com>
2022-10-08 18:10:32 +0800

    Conditional display of footer

--- hw3/src/container/footer.js
+++ hw3/src/container/footer.js
@@ -2,7 +2,7 @@
 
 function Footer(props){
     return(
-        <footer className = "todo-app__footer" id= "todo-footer"> 
+        <footer className = "todo-app__footer" id= "todo-footer" > 
             <div className="todo-app__total">{props.text}</div>
             <ul className="todo-app__view-buttons">
                 <button>All</button>
