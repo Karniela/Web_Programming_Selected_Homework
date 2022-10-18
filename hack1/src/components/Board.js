3fe97cc352495a562b43d3ff85a028dae79d1da4
Karniela <095282kitty@gmail.com>
2022-10-18 12:24:33 +0800

    gradescope version

--- hack1/src/components/Board.js
+++ hack1/src/components/Board.js
@@ -14,12 +14,12 @@ import CurRow from "./CurRow";
 const Board = ({ turn, guesses, curGuess }) => {
     return (
         <div className="Board-container">
-            <Row row_Idx="0" id="row_0" key="row_0"/>
-            <Row row_Idx="1" id="row_1" key="row_1"/>
-            <Row row_Idx="2" id="row_2" key="row_2"/>
-            <Row row_Idx="3" id="row_3" key="row_3"/>
-            <Row row_Idx="4" id="row_4" key="row_4"/>
-            <Row row_Idx="5" id="row_5" key="row_5"/>
+            <CurRow rowIdx="0" id="row_0" key="row_0" curGuess={curGuess}/>
+            <Row rowIdx="1" id="row_1" key="row_1" guess={guesses[0]}/>
+            <Row rowIdx="2" id="row_2" key="row_2" guess={guesses[1]}/>
+            <Row rowIdx="3" id="row_3" key="row_3" guess={guesses[2]}/>
+            <Row rowIdx="4" id="row_4" key="row_4" guess={guesses[3]}/>
+            <Row rowIdx="5" id="row_5" key="row_5" guess={guesses[4]}/>
             {/* TODO 2-2: show 6 rows (map function is recommended) and defined row's key.
                 Hint: Use `CurRow` instead of `Row` when you are passing `curGuess` into it. */}
             
--- hack1/src/components/CurRow.js
+++ hack1/src/components/CurRow.js
@@ -12,18 +12,17 @@ import React from 'react';
 
 const CurRow = ({ curGuess, rowIdx }) => {
     let letters = curGuess.split('');
-
     return (
         <div className='Row-container'>
             {/* TODO 3: Row Implementation -- CurRow */}
             
             {/* ↓ Default row, you should modify it. ↓ */}
             <div className='Row-wrapper current'>
-                <div className='Row-wordbox'></div>
-                <div className='Row-wordbox'></div>
-                <div className='Row-wordbox'></div>
-                <div className='Row-wordbox'></div>
-                <div className='Row-wordbox'></div>
+                <div className='Row-wordbox' id= {String(`${rowIdx}-0`)} key= {String(`${rowIdx}-0`)}>{letters[0]}</div>
+                <div className='Row-wordbox' id= {String(`${rowIdx}-1`)} key= {String(`${rowIdx}-1`)}>{letters[1]}</div>
+                <div className='Row-wordbox' id= {String(`${rowIdx}-2`)} key= {String(`${rowIdx}-2`)}>{letters[2]}</div>
+                <div className='Row-wordbox' id= {String(`${rowIdx}-3`)} key= {String(`${rowIdx}-3`)}>{letters[3]}</div>
+                <div className='Row-wordbox' id= {String(`${rowIdx}-4`)} key= {String(`${rowIdx}-4`)}>{letters[4]}</div>
             </div>
             {/* ↑ Default row, you should modify it. ↑ */}
         </div>
