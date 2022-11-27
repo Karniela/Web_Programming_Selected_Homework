c750b7c3350abdf8149b699241420a0bd22709dc
Karniela <095282kitty@gmail.com>
2022-11-27 22:49:19 +0800

    Stable Edition

--- hw7/backend/models/message.js
+++ hw7/backend/models/message.js
@@ -1,13 +1,16 @@
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema
-
-/******* Message Schema *******/
+// Creating a schema, sort of like working with an ORM
 const MessageSchema = new Schema({
-  chatBox: { type: mongoose.Types.ObjectId, ref: 'ChatBox' },
-  sender: { type: mongoose.Types.ObjectId, ref: 'User' },
-  body: { type: String, required: [true, 'Body field is required.'] },
-});
-const MessageModel = mongoose.model('Message', MessageSchema);
-
+  name: {
+    type: String,
+    required: [true, 'Name field is required.']
+}, body: {
+    type: String,
+    required: [true, 'Body field is required.']
+  }
+})
+// Creating a table within database with the defined schema
+const Message = mongoose.model('message', MessageSchema)
 // Exporting table for querying and mutating
-export default MessageModel
+export default Message;
