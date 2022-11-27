36c71e69babeecc513bb377aa41876f77542d79b
Karniela <095282kitty@gmail.com>
2022-11-27 20:07:34 +0800

    frontend done

--- hw7/frontend/src/containers/ChatRoom.js
+++ hw7/frontend/src/containers/ChatRoom.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import Title from '../components/Title.js'
 import Message from '../components/Message.js'
 import ChatModal from "../components/ChatModal.js"
-import { Input, Tag, Button, message } from 'antd'
+import { Input, Tabs, Button, message} from 'antd'
 import { useState, useEffect, useRef } from 'react'
 import {ChatProvider, useChat} from './hooks/useChat.js';
 
@@ -18,7 +18,14 @@ const ChatRoom = styled.div`
   margin: auto;
 `;
 
-const ChatBoxesWrapper = styled.div`
+const ChatBoxWrapper = styled.div`
+  height: calc(240px -36px);
+  display:flex;
+  flex-direction:column;
+  overflow: auto;
+`;
+
+const ChatBoxesWrapper = styled(Tabs)`
   width: 100%;
   height: 300px;
   background: #eeeeee52;
@@ -28,41 +35,81 @@ const ChatBoxesWrapper = styled.div`
   overflow: auto;
 `;
 
-function App() {
-  const { status, messages, sendMessage, clearMessages } = useChat()
-  const [username, setUsername] = useState('')
+const Chatroom=() => {
+  const {messages, sendMessage, clearMessages } = useChat()
+  const [chatBoxes, setChatBoxes] = useState([]); //{label, children, key}
   const [body, setBody] = useState('') // textBody
+  const [activeKey, setActiveKey] = useState('')
+  const [msgSent, setMsgSent] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
   const bodyRef = useRef(null)
   const { me, setMe, setSignedIn, displayStatus } = useChat();
+  
+  const renderChat = (chat) => (
+    chat.length === 0 ? (<p style={{ color: '#ccc' }}> No messages... </p>) : 
+    (<ChatBoxWrapper>{
+      chat.map
+      (({name, body}, i) =>
+      (<Message isMe={name===me} message={body} key={i}/>))}
+      </ChatBoxWrapper>))
+      // 產生 chat 的 DOM nodes
+  
+  const extractChat = (friend) => {
+      return renderChat(messages.filter(({name, body}) => ((name === friend) || (name === me))));
+  };
 
+  const createChatBox = (friend) => {
+    if (chatBoxes.some
+       (({key}) => key === friend)) {
+         throw new Error(friend + "'s chat box has already opened.");}
+
+    const chat = extractChat(friend);
+      setChatBoxes([...chatBoxes,{ label: friend, children: chat,key: friend }]);
+      setMsgSent(true);
+        return friend;
+  };
+
+  const removeChatBox =(targetKey, activeKey) => {
+    const index = chatBoxes.findIndex(({key}) => key === activeKey);
+    const newChatBoxes = chatBoxes.filter(({key}) =>key !== targetKey);
+    setChatBoxes(newChatBoxes);
+    return activeKey
+      ? activeKey === targetKey
+      ?index === 0
+      ?'' 
+      : chatBoxes[index - 1].key:activeKey
+      : '';
+    };
+  
   return (
     <ChatRoom>
-      <Title name = {me}> 
-      </Title>
+      <Title name = {me}/> 
+      <ChatBoxesWrapper
+        tabBarStyle={{height: '36px'}}
+        type="editable-card"
+        onChange={(key) => {
+          setActiveKey(key);
+          extractChat(key);
+        }}
+        onEdit={(targetKey, action) => {
+        if (action === 'add') setModalOpen(true);
+        else if (action === 'remove') {
+          setActiveKey(removeChatBox(targetKey, activeKey));
+        }
+        }}
+        items = {chatBoxes}
+        />
       
-      <ChatBoxesWrapper>
-        {messages.length === 0 ? (
-          <p style={{ color: '#ccc' }}> No messages... </p>
-        ) : (
-          messages.map(({ name, body }, i) => (
-            <p key={i}>
-              <Tag color="blue">{name}</Tag> {body}
-            </p>
-          ))
-        )}
-      </ChatBoxesWrapper>
-
-      <Input
-        placeholder={me}
-        value={me}
-        onChange={(e) => setUsername(e.target.value)}
-        style={{ marginBottom: 10 }}
-        onKeyDown={(e) => {
-          if (e.key === 'Enter') {
-            bodyRef.current.focus()
-        }}}
-      ></Input>
-      
+      <ChatModal
+      open={modalOpen}
+      onCreate={({ name }) => {
+      setActiveKey(createChatBox(name));
+      extractChat(name);
+      setModalOpen(false);
+      }}
+      onCancel={() => { setModalOpen(false);}}
+      />
+    
       <Input.Search
         ref={bodyRef}
         value={body}
@@ -70,14 +117,14 @@ function App() {
         enterButton="Send"
         placeholder="Type a message here..."
         onSearch={(msg) => {
-          if (!msg || !username) {
+          if (!msg) {
             displayStatus({
               type: 'error',
               msg: 'Please enter a username and a message body.'
             })
           return
           }
-        sendMessage({ name: username, body: msg })
+        sendMessage({ name: me, body: msg })
         setBody('')
         }}
       ></Input.Search>
@@ -86,4 +133,4 @@ function App() {
   )
 }
 
-export default App
+export default Chatroom;
